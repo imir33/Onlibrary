71192632734402d5c5345763ae0d066df95e807f
Ionut Iacob <ionut@Ubuntu20.04>
2020-11-11 21:05:07 +0200

    Added edit and delete books from profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -197,4 +197,101 @@ router.put(
   }
 );
 
+// @route    PUT api/profile/book/:book_id
+// @desc     Edit a book by id
+// @access   Private
+router.put(
+  '/book/:book_id',
+  [
+    [auth, checkObjectId('book_id')],
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('author', 'Author is required').not().isEmpty(),
+      check('numberOfPages', 'The number of pages is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      author,
+      numberOfPages,
+      currentPage,
+      finished,
+      from,
+      to,
+      rating,
+    } = req.body;
+
+    let newBook;
+
+    if (finished) {
+      newBook = {
+        title: title,
+        author: author,
+        numberOfPages: Number.parseInt(numberOfPages),
+        currentPage: Number.parseInt(numberOfPages),
+        finished: true,
+        rating: Number.parseInt(rating),
+        from: from,
+        to: to,
+      };
+    } else {
+      newBook = {
+        title: title,
+        author: author,
+        numberOfPages: Number.parseInt(numberOfPages),
+        currentPage: Number.parseInt(currentPage),
+        finished: false,
+        from: from,
+      };
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.books = profile.books.filter(
+        (book) => book._id.toString() !== req.params.book_id
+      );
+
+      profile.books.unshift(newBook);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route    DELETE api/profile/book/:book_id
+// @desc     Delete book from profile
+// @access   Private
+
+router.delete(
+  '/book/:book_id',
+  [auth, checkObjectId('book_id')],
+  async (req, res) => {
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.books = profile.books.filter(
+        (book) => book._id.toString() !== req.params.book_id
+      );
+
+      await profile.save();
+      return res.status(200).json(profile);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ msg: 'Server error' });
+    }
+  }
+);
+
 module.exports = router;
