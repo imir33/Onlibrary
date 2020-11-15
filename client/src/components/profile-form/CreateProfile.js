726c31e9fa298bbef1a374b937c22965c51b387c
Ionut Iacob <ionut@Ubuntu20.04>
2020-11-15 19:46:34 +0200

    Added the create profile form and action in the frontend

---
+++ client/src/components/profile-form/CreateProfile.js
@@ -0,0 +1,119 @@
+import React, { Fragment, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
+
+const CreateProfile = ({ createProfile, history }) => {
+  const [formData, setFormData] = useState({
+    location: '',
+    genres: '',
+    bio: '',
+    facebook: '',
+    instagram: '',
+  });
+
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
+  const { location, genres, bio, facebook, instagram } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Edit Your Profile</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Add some changes to your profile
+      </p>
+      <small>* = required field</small>
+      <form className='form' onSubmit={onSubmit}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Location'
+            name='location'
+            value={location}
+            onChange={onChange}
+          />
+          <small className='form-text'>
+            City & country suggested (eg. London, UK)
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='* Genres'
+            name='genres'
+            value={genres}
+            onChange={onChange}
+          />
+          <small className='form-text'>
+            Please use comma separated values (eg. Fantasy,Sci-Fi,Adventure)
+          </small>
+        </div>
+        <div className='form-group'>
+          <textarea
+            placeholder='A short bio of yourself'
+            name='bio'
+            value={bio}
+            onChange={onChange}
+          />
+          <small className='form-text'>Tell us a little about yourself</small>
+        </div>
+
+        <div className='my-2'>
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type='button'
+            className='btn btn-light'>
+            Add Social Network Links
+          </button>
+          <span>Optional</span>
+        </div>
+
+        {displaySocialInputs && (
+          <Fragment>
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x' />
+              <input
+                type='text'
+                placeholder='Facebook URL'
+                name='facebook'
+                value={facebook}
+                onChange={onChange}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x' />
+              <input
+                type='text'
+                placeholder='Instagram URL'
+                name='instagram'
+                value={instagram}
+                onChange={onChange}
+              />
+            </div>
+          </Fragment>
+        )}
+
+        <input type='submit' className='btn btn-primary my-1' />
+        <Link className='btn btn-light my-1' to='/dashboard'>
+          Go Back
+        </Link>
+      </form>
+    </Fragment>
+  );
+};
+
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
+
+export default connect(null, { createProfile })(withRouter(CreateProfile));
