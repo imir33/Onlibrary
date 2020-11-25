4a29095cb76dd43eacedb5b7e2ae16613e931a8d
Ionut Iacob <ionut@Ubuntu20.04>
2020-11-25 15:50:32 +0200

    Added send friend request route

--- routes/api/friends.js
+++ routes/api/friends.js
@@ -6,6 +6,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const Request = require('../../models/Request');
+const User = require('../../models/User');
 
 // @route    GET api/friends
 // @desc     Get current user friends
@@ -28,5 +29,41 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route    POST api/friends/request
+// @desc     Send a friend request to a user
+// @access   Private
+router.post(
+  '/request',
+  [auth, [check('email', 'Please include a valid email').isEmail()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email } = req.body;
+
+    try {
+      const user = await User.findOne({ email }).populate('user');
+
+      if (!user) {
+        return res.status(404).json({ errors: [{ msg: 'User not found' }] });
+      }
+
+      const request = new Request({
+        from: req.user.id,
+        to: user.id,
+      });
+
+      await request.save();
+
+      res.json(request);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
