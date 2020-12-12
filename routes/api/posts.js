bc5c488a3c79e1943cd48d57b3e739c56eb1a558
Ionut Iacob <ionut@Ubuntu20.04>
2020-12-12 20:59:49 +0200

    Added the delete post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -65,8 +65,37 @@ router.get('/:id', auth, async (req, res) => {
     }
 
     res.json(post);
-  } catch (errr) {
-    console.errorr(err.message);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).sendStatus('Server Errror');
+  }
+});
+
+// @route    DELETE api/posts/:id
+// @desc     Delete post by id
+// @access   Private
+
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return status(404).json({ msg: 'Post not found' });
+    }
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return status(401).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.error(err.message);
     if (err.kind === 'ObjectId') {
       return status(404).json({ msg: 'Post not found' });
     }
