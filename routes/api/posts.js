6afdfa50f7359ae912eb8b9ffcd37bca98c89ec8
Ionut Iacob <ionut@Ubuntu20.04>
2020-12-12 20:51:41 +0200

    Added get all posts and get post by id routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -39,4 +39,39 @@ router.post(
   }
 );
 
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (errr) {
+    console.errorr(err.message);
+    res.status(500).sendStatus('Server Errror');
+  }
+});
+
+// @route    GET api/posts/:id
+// @desc     Get post by id
+// @access   Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (errr) {
+    console.errorr(err.message);
+    if (err.kind === 'ObjectId') {
+      return status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).sendStatus('Server Errror');
+  }
+});
+
 module.exports = router;
