2d0be4f586b3089ba75350a4ec8173a7481f9f66
Ionut Iacob <ionut@Ubuntu20.04>
2020-12-13 15:33:37 +0200

    Modified wellcome message on the post page

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -14,7 +14,7 @@ const Posts = ({ getPosts, post: { posts } }) => {
     <Fragment>
       <h1 className='large text-primary'>Posts</h1>
       <p className='lead'>
-        <i className='fas fa-user' /> Welcome to the community
+        <i className='fas fa-user' /> Keep in touch with other book enthusiasts
       </p>
       <PostForm />
       <div className='posts'>
